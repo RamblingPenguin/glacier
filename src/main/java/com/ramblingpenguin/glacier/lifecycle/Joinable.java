a924e7c3d2d0ce1cc483c65c51a747f3e9c2317c
Dan Antal <dan@antal.io>
2025-12-19 15:57:38 -0800

    Deploy version 0.1.1.

--- src/main/java/com/ramblingpenguin/glacier/lifecycle/Joinable.java
+++ src/main/java/com/ramblingpenguin/glacier/lifecycle/Joinable.java
@@ -1,7 +1,5 @@
 package com.ramblingpenguin.glacier.lifecycle;
 
-import com.ramblingpenguin.glacier.observe.Listener;
-
 /**
  * Indicates that a component can be joined. Joining will cause the current thread to now block on the component.
  */
